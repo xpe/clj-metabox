be5cf6f51cea4b9d124f6593ec0b3351b1c422d2
David James <david@bluemontlabs.com>
2013-12-21 20:22:34 -0500

    add tests for nil and str

--- test/metabox/core_test.clj
+++ test/metabox/core_test.clj
@@ -4,17 +4,26 @@
             [metabox.core :refer (box val)]))
 
 (deftest metabox-test
+  (testing "(box)"
+    (let [mb (box)]
+      (is (= nil (.val mb)))
+      (is (= nil (val mb)))
+      (is (= nil (.meta mb)))
+      (is (= nil (meta mb)))
+      (is (= {:band false} (meta (with-meta mb {:band false}))))))
   (testing "(box 20)"
     (let [mb (box 20)]
       (is (= 20 (.val mb)))
       (is (= 20 (val mb)))
       (is (= nil (.meta mb)))
       (is (= nil (meta mb)))
-      (is (= {:band false} (meta (with-meta mb {:band false}))))))
+      (is (= {:band false} (meta (with-meta mb {:band false}))))
+      (is (= "20" (str mb)))))
   (testing "(box 20 {:band false})"
     (let [mb (box 20 {:band false})]
       (is (= 20 (.val mb)))
       (is (= 20 (val mb)))
       (is (= {:band false} (.meta mb)))
       (is (= {:band false} (meta mb)))
-      (is (= {} (meta (with-meta mb {})))))))
+      (is (= {} (meta (with-meta mb {}))))
+      (is (= "20" (str mb))))))
