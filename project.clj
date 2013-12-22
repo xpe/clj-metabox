caada886a4fd303dd928f37e9b20b5b2ef3853fe
David James <david@bluemontlabs.com>
2013-12-21 20:30:09 -0500

    add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metabox "0.1.0-SNAPSHOT"
   :description "Box a primitive with metadata in Clojure."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bluemont/clj-metabox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
